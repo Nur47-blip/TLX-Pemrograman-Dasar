98455160101a45127eb9c3085b4a5e5e6eddfdb6
Nur47-blip <67987788+Nur47-blip@users.noreply.github.com>
2020-10-27 17:18:15 +0700

    Create LuasSegitiga.cpp

---
+++ LuasSegitiga.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+	int a,b;
+	cin>>a>>b;
+	cout<<setprecision(2)<<fixed;
+	double c;
+	c=(float)a*(float)b/2;
+	cout<<c<<endl;
+
+return 0;
+}

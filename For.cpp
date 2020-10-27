85b74f38cfaa3f14379af51dbbb3d48e091b9535
Nur47-blip <67987788+Nur47-blip@users.noreply.github.com>
2020-10-27 17:29:16 +0700

    Create For.cpp

---
+++ For.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+	int a,b;
+		int j;
+		b=0;
+	cin>>a;
+	for (int i=1;i<=a;i++){
+
+	cin>>j;
+	b=b+j;
+}
+cout<<b<<endl;
+
+	
+
+return 0;
+}

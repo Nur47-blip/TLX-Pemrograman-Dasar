217a2151733131a80b26e9629c54021162d4424e
Nur47-blip <67987788+Nur47-blip@users.noreply.github.com>
2020-10-27 17:32:36 +0700

    Create PolaI.cpp

---
+++ PolaI.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+	int a,b;
+	cin>>a>>b;
+	for (int i=1;i<=a;i++){
+		if (i%b==0) cout<<'*';
+		else cout<<i;
+		if (i<a) cout<<' ';
+		else cout<<endl;
+	}
+
+return 0;
+}
+

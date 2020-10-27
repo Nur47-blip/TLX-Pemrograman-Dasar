8044fd792d41e2693f5c9e020c64d48960ca777c
Nur47-blip <67987788+Nur47-blip@users.noreply.github.com>
2020-10-27 17:38:30 +0700

    Create BalikDaftar.cpp

---
+++ BalikDaftar.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+	int arr[100];
+	int i=0;
+	
+	while (cin >> arr[i]){
+		i++;
+	}
+	
+	i--;
+	while (i>=0){
+		cout << arr[i] << endl;
+		i--;
+	}
+
+return 0;
+}

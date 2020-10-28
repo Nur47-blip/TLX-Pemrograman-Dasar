99233803d8d9e4e60e07ad8aed14e0d2a9810cfe
Nur47-blip <67987788+Nur47-blip@users.noreply.github.com>
2020-10-29 00:06:57 +0700

    Add files via upload

---
+++ KonversiBiner.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string biner(int n){
+	if (n==0) return "0";
+	if (n==1) return "1";
+	else {
+		if (n%2==0) return biner(n/2)+"0";
+		else return biner(n/2)+"1";
+	}
+}
+
+int main(){
+
+	int n;
+	cin >> n;
+	
+	cout << biner(n) << endl;
+
+	return 0;
+}
+

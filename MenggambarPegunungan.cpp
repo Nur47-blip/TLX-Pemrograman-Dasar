3193b227d148aba4882f085d9172c04f900e7646
Nur47-blip <67987788+Nur47-blip@users.noreply.github.com>
2020-10-29 00:10:10 +0700

    Add files via upload

---
+++ MenggambarPegunungan.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void gambar(int n){
+	if (n==1){
+		cout << "*\n";
+	}
+	else {
+		gambar(n-1);
+		for (int i = 0; i < n; i++){
+			cout << "*";
+		}
+		cout << endl;
+		gambar(n-1);
+	}
+}
+
+int main(){
+
+	int n;
+	cin >> n;
+	
+	gambar(n);
+
+	return 0;
+}
+

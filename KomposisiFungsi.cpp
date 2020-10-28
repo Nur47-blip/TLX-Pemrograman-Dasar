ff9f81d96a47b83ddfba405f7720f054aabc8b8e
Nur47-blip <67987788+Nur47-blip@users.noreply.github.com>
2020-10-28 22:30:56 +0700

    Add files via upload

---
+++ KomposisiFungsi.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(int a, int b, int k, int x){
+	if (k==1) return abs(a*x+b);
+	if (k > 0) return f(a, b, k-1, abs(a*x+b));
+}
+
+int main(){
+
+	int a, b, k, x;
+	cin >> a >> b >> k >> x;
+	
+	cout << f(a,b,k,x) << endl;
+
+	return 0;
+}
+
